bd75ab126a681fb45bc7b292e6013c0e60264f77
alexandr-lebedko <lebedko.alexandr@gmail.com>
2018-04-02 14:52:44 +0200

    Refactored CreatePaperCheckStatusTransition class
    -username and password retrieved from  method arguments
    -programToken, userToken and paperCheckToken retrieved from method arguments if present or else default values set

--- src/main/java/com/hyperwallet/example/papercheck/CreatePaperCheckStatusTransition.java
+++ src/main/java/com/hyperwallet/example/papercheck/CreatePaperCheckStatusTransition.java
@@ -2,26 +2,29 @@ package com.hyperwallet.example.papercheck;
 
 import com.hyperwallet.clientsdk.Hyperwallet;
 import com.hyperwallet.clientsdk.model.HyperwalletStatusTransition;
-import org.apache.commons.lang3.builder.MultilineRecursiveToStringStyle;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import com.hyperwallet.example.Util;
 
 public class CreatePaperCheckStatusTransition {
 
     public static void main(String[] args) throws Exception {
-        String username = System.getProperty("HYPERWALLET_USERNAME");
-        String password = System.getProperty("HYPERWALLET_PASSWORD");
+        int i = 0;
+        String username = args[i++];
+        String password = args[i++];
+        String programToken = (args.length > i) ? args[i++] : "prg-44a64920-45fc-4578-a7f9-e14813b3ed54";
+        String userToken = (args.length > i) ? args[i++] : "usr-b4a1f0bd-cbf7-4467-aa6f-4445dea7e67d";
+        String paperCheckToken = (args.length > i) ? args[i++] : "trm-b379ca8e-973a-47d4-a79c-eaaf2af89ee6";
 
         Hyperwallet client = new Hyperwallet(username,
                                              password,
-                                             "prg-44a64920-45fc-4578-a7f9-e14813b3ed54");
+                                             programToken);
 
         HyperwalletStatusTransition transition = new HyperwalletStatusTransition();
         transition.setTransition(HyperwalletStatusTransition.Status.DE_ACTIVATED);
 
-        HyperwalletStatusTransition statusTransition = client.createPaperCheckStatusTransition("usr-b4a1f0bd-cbf7-4467-aa6f-4445dea7e67d",
-                                                                                             "trm-b379ca8e-973a-47d4-a79c-eaaf2af89ee6",
-                                                                                             transition);
+        HyperwalletStatusTransition statusTransition = client.createPaperCheckStatusTransition(userToken,
+                                                                                               paperCheckToken,
+                                                                                               transition);
 
-        System.out.println(ToStringBuilder.reflectionToString(statusTransition, new MultilineRecursiveToStringStyle()));
+        Util.print(statusTransition);
     }
 }
