2c3403a24dbff0eece3ba44dce9d042087cf972b
Artem_Pryzhkov <artem_pryzhkov@epam.com>
2018-04-06 13:03:50 +0300

    add user status transition examples for "get" and "list" methods

---
+++ src/main/java/com/hyperwallet/example/user/GetUserStatusTransition.java
@@ -0,0 +1,27 @@
+package com.hyperwallet.example.user;
+
+import com.hyperwallet.clientsdk.Hyperwallet;
+import com.hyperwallet.clientsdk.model.HyperwalletStatusTransition;
+import com.hyperwallet.example.Util;
+
+public class GetUserStatusTransition {
+
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+        String username = args[i++];
+        String password = args[i++];
+        String programToken = (args.length > i) ? args[i++] : "prg-44a64920-45fc-4578-a7f9-e14813b3ed54";
+        String userToken = (args.length > i) ? args[i++] : "usr-0fb6d624-f18d-480d-b1e7-f48f534bbd75";
+        String statusTransitionToken = (args.length > i) ? args[i++] : "";
+
+        Hyperwallet client = new Hyperwallet(username,
+                                             password,
+                                             programToken);
+
+        HyperwalletStatusTransition
+            statusTransition =
+            client.getUserStatusTransition(userToken, statusTransitionToken);
+
+        Util.print(statusTransition);
+    }
+}
